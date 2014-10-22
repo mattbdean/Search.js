c7c2d434726626553ecae7ab1364e433f574b4a2
Matthew Dean <deanmatthew16@gmail.com>
2014-10-22 00:11:50 -0400

    Add build

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-    echo "Which folder do you want to deploy to GitHub Pages?"
-    exit 1
-fi
-
-git subtree push --prefix $1 origin gh-pages
+grunt && git subtree push --prefix "build/" gh-pages origin
