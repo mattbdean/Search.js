83194951c8da89106ee147716a95a5f55a246b4e
Matthew Dean <deanmatthew16@gmail.com>
2014-10-23 19:48:01 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-grunt && git subtree push --prefix "build/" gh-pages origin
+set -e
+shopt -s extglob
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+grunt # Build the website
+cp "build/" -r .. # Move the build dir to safety
+git checkout gh-pages
+rm !(.git*|.|..) -rf # Remove all but .git, the current and parent dirs
+mv ../build/* ./ # Move the contents of the build dir here
+rm -r ../build/ # ... and then remove it
+git add . 
+git commit -m "Update gh-pages"
+git push
+git checkout master
+
